6200665e7997052cfc17b5b71a286fc399f1f596
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2023-03-31 11:39:31 -0300

    Defasio POO Smartphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia("123","Nokia S plus", "0DA12H", 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Iphone iphone = new Iphone("987","Iphone 13 pro", "0ACD12", 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
